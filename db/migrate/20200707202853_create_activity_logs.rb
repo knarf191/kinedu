30a44e86e7e4fc4afce82d5326449c6e930f09fc
Frank Reyes <frank_reyesm@hotmail.com>
2020-07-09 22:26:38 +0000

    Change the name of column asistand_id to asistant_id of activity logs

--- db/migrate/20200707202853_create_activity_logs.rb
+++ db/migrate/20200707202853_create_activity_logs.rb
@@ -2,7 +2,7 @@ class CreateActivityLogs < ActiveRecord::Migration[6.0]
   def change
     create_table :activity_logs do |t|
       t.integer :baby_id
-      t.integer :assistand_id
+      t.integer :assistant_id
       t.integer :activity_id
       t.datetime :start_time
       t.datetime :stop_time
